59e1754293c72167282fca2c3c8f6e829ce7342e
Will Gautreaux <gautreauxwill@gmail.com>
2022-10-20 00:49:13 -0400

    working on routing pages

--- app/my-app/src/pages/BookingPage/BookingPage.js
+++ app/my-app/src/pages/BookingPage/BookingPage.js
@@ -1,12 +1,44 @@
 import React from "react";
 import GlobalNavBar from "../GlobalNavBar/GlobalNavBar";
 import {useLocation} from 'react-router-dom';
+import { Grid } from '@mui/material'
+import Seating from "./Seating";
 
 
-
-const avatarShowtimes = [
+const avatarShowtimesArr = [
     { room: 2, startTime: '22:00', endTime: '23:30' },
     { room: 5, startTime: '23:00', endTime: '24:30' }
+]
+
+// Array of objects with seat number and availability
+const seatsArr = [
+    { number: 1, available: true },
+    { number: 2, available: true },
+    { number: 3, available: true },
+    { number: 4, available: true },
+    { number: 5, available: true },
+    { number: 6, available: true },
+    { number: 7, available: true },
+    { number: 8, available: true },
+    { number: 9, available: true },
+    { number: 10, available: false },
+    { number: 11, available: true },
+    { number: 12, available: true },
+    { number: 13, available: true },
+    { number: 14, available: true },
+    { number: 15, available: true },
+    { number: 16, available: false },
+    { number: 17, available: false },
+    { number: 18, available: true },
+    { number: 19, available: true },
+    { number: 20, available: true },
+    { number: 21, available: true },
+    { number: 22, available: true },
+    { number: 23, available: true },
+    { number: 24, available: true },
+    { number: 25, available: true },
+    { number: 26, available: true },
+    { number: 27, available: true },
 ]
 
 const BookingPage = (props) => {
@@ -16,7 +48,8 @@ const BookingPage = (props) => {
     const title = location.state.title
     const rating = location.state.rating
     const genre = location.state.genre
-    const showtimes = avatarShowtimes
+    const showtimes = avatarShowtimesArr
+    const seats = seatsArr
 
     console.log(title)
     // Two columns in middle of screen
@@ -39,10 +72,14 @@ const BookingPage = (props) => {
     return (
         <div>
             <GlobalNavBar/>
-            <p>
-                {title}
-            </p>
+            <Grid container spacing={2}>
+                <Grid item xs={7}>
+                    <Seating seats={seats} />
+                </Grid>
+                <Grid item xs={5}>
 
+                </Grid>
+            </Grid>
         </div>
     )
 }
---
+++ app/my-app/src/pages/BookingPage/Seating.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Seating = (props) => {
+
+    const seats = props.seats
+
+    console.log(seats)
+
+    // React component with five rows that each contain a number of seats
+    return (
+        <div>
+            <p>Something</p>
+        </div>
+    )
+
+
+}
+
+export default Seating
