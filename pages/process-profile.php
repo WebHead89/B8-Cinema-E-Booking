00b09c8bd5fca6820e59a97978f0e2608c6a47ff
nseverson6425 <nickseverson@me.com>
2022-10-25 19:06:46 -0400

    finished edit profile, need add credit card info

--- pages/process-profile.php
+++ pages/process-profile.php
@@ -1,32 +1,20 @@
 <?php 
+session_start();
 
-ini_set("SMTP", "smtp.gmail.com");
-ini_set("smtp_port", "587");
+// setting all variables from $_POST
+$first_name=$_POST["first_name"];
+$last_name=$_POST["last_name"];
+$phone=$_POST["phone"];
+$address=$_POST["address"];
+$city=$_POST["city"];
+$state=$_POST["state"];
+$zip=$_POST["zip"];
+$id=$_SESSION["user_id"];
 
-// Server-side validation to make sure name is not NULL
-if (empty($_POST["first_name"])) {
-    die("First name is requied");
-}
-if (empty($_POST["last_name"])) {
-    die("Last name is requied");
-}
+// Password hash to store credit card # securely
+$cc_hash = password_hash($_POST["cc_number"], PASSWORD_DEFAULT);
 
-// Server-side validation to check for valid email
-if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-    die("Valid email is required");
-}
-
-// Server-side validation to see if password is more than 8 char
-if (strlen($_POST["password"]) < 8) {
-    die("Password must be at least 8 characters");
-}
-
-// Validation to check if passwords match
-if ($_POST["password"] !== $_POST["password_confirmation"]) {
-    die("Passwords must match");
-}
-
-// Password hash to store passwords securely
+// Password hash to store password securely
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 // Set promo value to a variable
@@ -36,33 +24,20 @@ if ($_POST["promo"] == "Yes") {
     $promo = 0;
 }
 
-// Generates random 32 char hash for email verification
-$hash = md5( rand(0, 1000));
-echo $hash;
 
 // require database.php for database connection
 $mysqli = require __DIR__ . "/database.php";
 
-// sql insert statement to insert into the database
-$sql = "INSERT INTO user(first_name, last_name, phone, email, password, promo, emailHash, status)
-        VALUES(?, ?, ?, ?, ?, ?, ?, 2)";
-
-// init for sql execution
+// sql insert statement to update the database 
 $stmt = $mysqli->stmt_init();
+$stmt = $mysqli->prepare("UPDATE user SET first_name='$first_name', last_name='$last_name', phone='$phone', password='$password_hash', promo='$promo', address='$address', city='$city', state='$state', zip='$zip' WHERE id='$id'");
 
-// print error statement and die if problems preparing
-if ( ! $stmt->prepare($sql)) {
-    die("SQL error: " . $mysqli->error);
+
+
+
+if($stmt->execute()) {
+    header("Location: signup-success.html");
+    exit;
 }
-
-// binding params to be added
-$stmt->bind_param("sssssis",
-                  $_POST['first_name'],
-                  $_POST['last_name'],
-                  $_POST['phone'],
-                  $_POST['email'],
-                  $password_hash,
-                  $promo,
-                  $hash);
 
 ?>
