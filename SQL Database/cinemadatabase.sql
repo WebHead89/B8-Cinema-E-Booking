163bfbf69269ca20cbcc3c3e9ee0d8320ac0b1cc
Ben Prestel <benjamindprestel@gmail.com>
2022-10-25 20:47:19 -0400

    Update cinemadatabase.sql

--- SQL Database/cinemadatabase.sql
+++ SQL Database/cinemadatabase.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 23, 2022 at 10:53 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: localhost:3307
+-- Generation Time: Oct 26, 2022 at 02:46 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cinemadatabase`
+-- Database: `cinema_ebooking_system`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `account_status` (
   `idStatus` int(11) NOT NULL,
   `status` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tell Users table what the status of an acount is';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tell Users table what the status of an acount is';
 
 --
 -- Dumping data for table `account_status`
@@ -50,7 +50,7 @@ INSERT INTO `account_status` (`idStatus`, `status`) VALUES
 CREATE TABLE `account_type` (
   `idType` int(11) NOT NULL,
   `type` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tell users table what account type the user is';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tell users table what account type the user is';
 
 --
 -- Dumping data for table `account_type`
@@ -73,7 +73,7 @@ CREATE TABLE `booking_table` (
   `showID` int(11) NOT NULL,
   `paymentID` int(11) NOT NULL,
   `promoID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store all of the bookings for the movie theater\n';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store all of the bookings for the movie theater\n';
 
 -- --------------------------------------------------------
 
@@ -92,7 +92,7 @@ CREATE TABLE `movies_table` (
   `trailerVideo` varchar(45) DEFAULT NULL,
   `filmRating` varchar(45) DEFAULT NULL,
   `categoryID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store all information on a movie';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store all information on a movie';
 
 -- --------------------------------------------------------
 
@@ -103,7 +103,7 @@ CREATE TABLE `movies_table` (
 CREATE TABLE `movie_category` (
   `idCategory` int(11) NOT NULL,
   `category` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores the different categories of movies';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='stores the different categories of movies';
 
 -- --------------------------------------------------------
 
@@ -115,8 +115,9 @@ CREATE TABLE `payment_card_table` (
   `idPaymentCard` int(11) NOT NULL,
   `cardNum` varchar(45) NOT NULL,
   `experationDate` varchar(45) NOT NULL,
-  `idUser` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store payment cards of the users';
+  `billingAddress` varchar(100) DEFAULT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store payment cards of the users';
 
 -- --------------------------------------------------------
 
@@ -128,7 +129,7 @@ CREATE TABLE `promotions_table` (
   `idPromotions` int(11) NOT NULL,
   `code` varchar(45) NOT NULL,
   `discount` decimal(2,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store promotions';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store promotions';
 
 -- --------------------------------------------------------
 
@@ -142,7 +143,7 @@ CREATE TABLE `review_table` (
   `review` varchar(500) NOT NULL,
   `movieID` int(11) NOT NULL,
   `userID` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores reviews for movies';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='stores reviews for movies';
 
 -- --------------------------------------------------------
 
@@ -154,7 +155,7 @@ CREATE TABLE `seats_table` (
   `seatNumber` int(11) NOT NULL,
   `isReserved` tinyint(4) DEFAULT 0,
   `showID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='holds the all seats for a show, all seats are generated when a new show is created';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='holds the all seats for a show, all seats are generated when a new show is created';
 
 -- --------------------------------------------------------
 
@@ -166,7 +167,7 @@ CREATE TABLE `showroom_table` (
   `idRoom` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `seatNumber` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store all of the showrooms in the threater';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store all of the showrooms in the threater';
 
 -- --------------------------------------------------------
 
@@ -177,7 +178,7 @@ CREATE TABLE `showroom_table` (
 CREATE TABLE `showtime_table` (
   `idShowtime` int(11) NOT NULL,
   `showtime` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store the showtimes for the theater';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store the showtimes for the theater';
 
 -- --------------------------------------------------------
 
@@ -191,7 +192,7 @@ CREATE TABLE `show_table` (
   `movieID` int(11) NOT NULL,
   `showroomID` int(11) NOT NULL,
   `showtimeID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='table to store all the shows in a movie';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table to store all the shows in a movie';
 
 -- --------------------------------------------------------
 
@@ -204,7 +205,7 @@ CREATE TABLE `tickets_table` (
   `bookingID` int(11) NOT NULL,
   `typeID` int(11) NOT NULL,
   `seatNumber` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='tables to store all of the tickets';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='tables to store all of the tickets';
 
 -- --------------------------------------------------------
 
@@ -216,12 +217,12 @@ CREATE TABLE `ticket_type` (
   `idType` int(11) NOT NULL,
   `type` varchar(45) DEFAULT NULL,
   `price` decimal(4,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores the different types of tickets\n';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='stores the different types of tickets\n';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_table`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -237,8 +238,47 @@ CREATE TABLE `user` (
   `zip` varchar(255) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `admin` tinyint(1) NOT NULL DEFAULT 0,
-  `emailHash` varchar(32) DEFAULT NULL
+  `emailHash` varchar(32) DEFAULT NULL,
+  `address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `phone`, `email`, `password`, `promo`, `city`, `state`, `zip`, `status`, `admin`, `emailHash`, `address`) VALUES
+(1, 'Test', '', '1234567890', 'test@test.com', '$2y$10$sKmAmFPogTsC6pUIMO/8eu4YNl9G3xzxrSRf/6Uyf4NkS2qr98FVS', 0, '', '', '', 0, 0, NULL, ''),
+(2, 'test2', '', '987654321', 'test2@test.com', '$2y$10$tZhSQV3qUItCyQ2.c6byrOUYrnr7X2lEsUctI/R.Xk8jX/htGohnK', 0, '', '', '', 0, 0, NULL, ''),
+(3, 'test2', '', '987654321', 'test3@test.com', '$2y$10$1RE7I/DzLunm3QWzpbxpd.7DMIrYI0tbbNzYP2K23YvsmL1rXXYcO', 0, '', '', '', 0, 0, NULL, ''),
+(4, 'test4', '', '987654321', 'test4@test.com', '$2y$10$4LFoeUeMkurMUQxzG3d46.bJD4AuNPtDpYNOvOmXprFMKcJNbCvvC', 0, '', '', '', 0, 0, NULL, ''),
+(8, 'Nick', 'Severson', '1234567890', 'nickseverson@me.com', '$2y$10$xL1XFGGhwdKnIEnby2RVIe4bmfHzwSxfXaiZNMePKkH.XWfkyEaZe', 0, NULL, NULL, NULL, 0, 0, NULL, ''),
+(9, 'Tucker', 'Folsom', '1234567890', 'tucker@treats.com', '$2y$10$UX4i/OvMTFnmDO0SNHsiaOoYuhSKEzwh9KlTlcLaoCLHPv6uxrT8i', 0, NULL, NULL, NULL, 0, 0, NULL, ''),
+(11, 'Tucker', 'Folsom', '1234567890', 'tucker2@treats.com', '$2y$10$aogyfuaJHUyUXs9DtmqE1OOwLGHKCJdq646i7gT/4b6VkVLDQhb1W', 1, NULL, NULL, NULL, 0, 0, NULL, ''),
+(12, 'Tucker', 'Folsom', '1234567890', 'tucker3@treats.com', '$2y$10$92814ycftqjTDHVWBJJJCu6u.xAjyPVmL6EsvX2Zq87f4xhSCD6qO', 0, NULL, NULL, NULL, 0, 0, NULL, ''),
+(14, 'ben', 'prestel', '7703304466', 'bdp67818@uga.edu', '$2y$10$ijNrf6uqudwJr6EnTy8tdOpFJRfxLwRt6zJ8vd0WI9T8TnboN/SLm', 1, NULL, NULL, NULL, 2, 0, NULL, ''),
+(15, 'Ben ', 'Prestel', '7703304466', 'benjamin43.prestel@gmail.com', '$2y$10$3x5.EV3IjgNNKONnLsLMdegWwwM4CSkA5n1bZuvjyMJtphY1Mz4Bu', 1, 'Marietta', '', '', 1, 0, '72da7fd6d1302c0a159f6436d01e9eb0', 'hello'),
+(16, 'Ben', 'Prestel', '7703304466', 'ben@athensbulldogs.com', '$2y$10$X4jwS8mLB9IDkB8H6YpOAu4N8MptyJU6Wffsrsn0baFrtxO9kfDGu', 1, NULL, NULL, NULL, 2, 0, '89fcd07f20b6785b92134bd6c1d0fa42', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `idUser` int(10) UNSIGNED NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `recievePromo` tinyint(4) NOT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `zipcode` varchar(45) DEFAULT NULL,
+  `accountStatus` int(11) DEFAULT NULL,
+  `accountType` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table to store all the users of the system\n';
 
 --
 -- Indexes for dumped tables
@@ -283,8 +323,7 @@ ALTER TABLE `movie_category`
 -- Indexes for table `payment_card_table`
 --
 ALTER TABLE `payment_card_table`
-  ADD PRIMARY KEY (`idPaymentCard`),
-  ADD KEY `idUser_idx` (`idUser`);
+  ADD PRIMARY KEY (`idPaymentCard`);
 
 --
 -- Indexes for table `promotions_table`
@@ -342,6 +381,22 @@ ALTER TABLE `tickets_table`
 --
 ALTER TABLE `ticket_type`
   ADD PRIMARY KEY (`idType`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `idUsers_UNIQUE` (`idUser`),
+  ADD KEY `accountType_idx` (`accountType`),
+  ADD KEY `accountStatus_idx` (`accountStatus`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -426,6 +481,18 @@ ALTER TABLE `ticket_type`
   MODIFY `idType` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -443,12 +510,6 @@ ALTER TABLE `booking_table`
 --
 ALTER TABLE `movies_table`
   ADD CONSTRAINT `categoryType` FOREIGN KEY (`categoryID`) REFERENCES `movie_category` (`idCategory`);
-
---
--- Constraints for table `payment_card_table`
---
-ALTER TABLE `payment_card_table`
-  ADD CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`);
 
 --
 -- Constraints for table `review_table`
@@ -478,6 +539,13 @@ ALTER TABLE `tickets_table`
   ADD CONSTRAINT `bookingID` FOREIGN KEY (`bookingID`) REFERENCES `booking_table` (`idBooking`),
   ADD CONSTRAINT `seatNumber` FOREIGN KEY (`seatNumber`) REFERENCES `seats_table` (`seatNumber`),
   ADD CONSTRAINT `typeID` FOREIGN KEY (`typeID`) REFERENCES `ticket_type` (`idType`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `accountStatus` FOREIGN KEY (`accountStatus`) REFERENCES `account_status` (`idStatus`),
+  ADD CONSTRAINT `accountType` FOREIGN KEY (`accountType`) REFERENCES `account_type` (`idType`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
